4c560cae38c32c12eb6f20d8e9bee7d05ff1587f
Jonathan <jortiz@tunein.com>
2016-03-17 17:15:54 -0700

    app.js

--- submit/js/app.js
+++ submit/js/app.js
@@ -5,3 +5,8 @@ $(document).ready(function() {
   $("#time").text(Date.now());
 
 });
+
+
+function rick(){
+  $('div').append('<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" name="frame1" id="frame1"></iframe>');
+}
