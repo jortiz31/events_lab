aad147bd1cde98628f2307198fe8806433478803
Jonathan <jortiz@tunein.com>
2016-03-18 00:50:50 -0700

    app.js

--- change/js/app.js
+++ change/js/app.js
@@ -5,3 +5,21 @@ $(document).ready(function() {
   $("#time").text(Date.now());
 
 });
+
+$(document).ready(function() {
+    $(".sum").val("0");
+    $(".key").val("");
+
+    function calc() {
+        //function removes all newlines, spaces (including non-breaking spaces), and tabs from the beginning and end of the supplied string.
+        //If these whitespace characters occur in the middle of the string, they are preserved.
+        var $num1 = ($.trim($(".num1").val()) != "" && !isNaN($(".num1").val())) ? parseInt($(".num1").val()) : 0;
+        console.log($num1);
+        var $num2 = ($.trim($(".num2").val()) != "" && !isNaN($(".num2").val())) ? parseInt($(".num2").val()) : 0;
+        console.log($num2);
+        $(".sum").val($num1 + $num2);
+    }
+    $(".key").keyup(function() {
+        calc();
+    });
+});
